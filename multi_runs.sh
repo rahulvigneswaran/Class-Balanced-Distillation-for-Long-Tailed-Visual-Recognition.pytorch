5eff759bb86c5479ebebe6d9a309130a79c7cf25
Rahul Vigneswaran <37276766+rahulvigneswaran@users.noreply.github.com>
2021-10-31 22:32:46 +0530

    Update multi_runs.sh

--- multi_runs.sh
+++ multi_runs.sh
@@ -15,7 +15,7 @@ for seeds in 1
 do
     for alphas in 0.2 0.4 0.8
     do
-        betas in 50 100 200
+        for betas in 50 100 200
         do
             python main.py --experiment=0.3 --alpha=$alphas --beta=$betas --seed=$seeds --gpu="0,1" --log_offline --normal_teacher="10,20" --aug_teacher="20,30"
         done
